635eb825ab4005fb2afc5b8dc4cf21940380ae1b
Michael Kapelko <kornerr@gmail.com>
2015-10-12 20:44:43 +0700

    #2299 Recreate the test chamber - Main2 listens to reports

--- scripts/Button.py
+++ scripts/Button.py
@@ -7,18 +7,34 @@ BUTTON_ACTION_PRESS_GROUP = "default"
 BUTTON_ACTION_PRESS_NAME  = "moveButtonDown"
 
 class ButtonImpl(object):
-    def __init__(self, scene, action):#, listeners):
+    def __init__(self, scene, action, senv):
         # Refer.
-        self.scene     = scene
-        self.action    = action
-        #self.listeners = listeners
+        self.scene  = scene
+        self.action = action
+        self.senv   = senv
         # Create.
         self.selectable = {}
+        self.actions    = {}
     def __del__(self):
         # Derefer.
-        self.scene     = None
-        self.action    = None
-        #self.listeners = None
+        self.scene  = None
+        self.action = None
+        self.senv   = None
+    def onActionState(self, actionName, state):
+        # Ignore activation.
+        if (state):
+            return
+        # Ignore other actions.
+        if (actionName not in self.actions):
+            return
+        print "onActionState", actionName, state
+        # Report.
+        st = pymjin2.State()
+        key = "button.{0}.selected".format(self.actions[actionName])
+        st.set(key, "1")
+        self.senv.reportStateChange(st)
+        st.set(key, "0")
+        self.senv.reportStateChange(st)
     def onSelection(self, sceneName, nodeName):
         if ((sceneName not in self.selectable) or
             (nodeName not in self.selectable[sceneName])):
@@ -43,9 +59,26 @@ class ButtonImpl(object):
                                                  newGroupName,
                                                  BUTTON_ACTION_PRESS_NAME)
             self.selectable[sceneName][nodeName] = newActionName
+            self.actions[newActionName] = "{0}.{1}".format(sceneName, nodeName)
         # Remove disabled.
         elif (nodeName in self.selectable[sceneName]):
+            actionName = self.selectable[sceneName][nodeName]
+            del self.actions[actionName]
             del self.selectable[sceneName][nodeName]
+
+class ButtonListenerAction(pymjin2.ComponentListener):
+    def __init__(self, impl):
+        pymjin2.ComponentListener.__init__(self)
+        # Refer.
+        self.impl = impl
+    def __del__(self):
+        # Derefer.
+        self.impl = None
+    def onComponentStateChange(self, st):
+        for k in st.keys:
+            actionName = k.replace(".active", "")
+            state = (st.value(k)[0] == "1")
+            self.impl.onActionState(actionName, state)
 
 class ButtonListenerSelection(pymjin2.ComponentListener):
     def __init__(self, impl):
@@ -94,19 +127,25 @@ class Button:
         self.action = action
         self.senv   = scriptEnvironment
         # Create.
-        self.impl              = ButtonImpl(scene, action)
+        self.impl              = ButtonImpl(scene, action, scriptEnvironment)
+        self.listenerAction    = ButtonListenerAction(self.impl)
         self.listenerSelection = ButtonListenerSelection(self.impl)
         self.extension         = ButtonExtensionScriptEnvironment(self.impl)
         # Prepare.
         key = "selector..selectedNode"
         self.scene.addListener([key], self.listenerSelection)
+        key = "{0}..{1}.active".format(BUTTON_ACTION_PRESS_TYPE,
+                                       BUTTON_ACTION_PRESS_NAME)
+        self.action.addListener([key], self.listenerAction)
         self.senv.addExtension(self.extension)
         print "{0} Button.__init__".format(id(self))
     def __del__(self):
         # Tear down.
+        self.action.removeListener(self.listenerAction)
         self.scene.removeListener(self.listenerSelection)
         self.senv.removeExtension(self.extension)
         # Destroy.
+        del self.listenerAction
         del self.listenerSelection
         del self.extension
         del self.impl
--- scripts/Main2.py
+++ scripts/Main2.py
@@ -3,6 +3,18 @@ import pymjin2
 
 MAIN2_DEPENDENCY_BUTTON = "scripts/Button.py"
 
+class Main2ListenerScriptEnvironment(pymjin2.ComponentListener):
+    def __init__(self, impl):
+        pymjin2.ComponentListener.__init__(self)
+        # Refer.
+        self.impl = impl
+    def __del__(self):
+        # Derefer.
+        self.impl = None
+    def onComponentStateChange(self, st):
+        for k in st.keys:
+            print k, st.value(k)
+
 class Main2:
     def __init__(self,
                  sceneName,
@@ -20,6 +32,7 @@ class Main2:
         # Create.
         module = self.dependencies[MAIN2_DEPENDENCY_BUTTON]
         self.button = module.Button(scene, action, scriptEnvironment)
+        self.listenerSEnv = Main2ListenerScriptEnvironment(None)
         # Prepare.
         # Mark buttons selectable.
         buttons = ["seControlRight", "seControlLeft", "seControlUp", "seControlDown"]
@@ -28,8 +41,12 @@ class Main2:
             key = "button.{0}.{1}.selectable".format(sceneName, b)
             st.set(key, "1")
         self.senv.setState(st)
+        # Listen to button selection.
+        self.senv.addListener(["button...selected"], self.listenerSEnv)
         print "{0} Main2.__init__({1}, {2})".format(id(self), sceneName, nodeName)
     def __del__(self):
+        # Tear down.
+        self.senv.removeListener(self.listenerSEnv)
         # Derefer.
         self.scene        = None
         self.action       = None
@@ -37,6 +54,7 @@ class Main2:
         self.dependencies = None
         # Destroy
         del self.button
+        del self.listenerSEnv
         print "{0} Main2.__del__".format(id(self))
 
 def SCRIPT_CREATE(sceneName,
