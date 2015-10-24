482cd95639caf9d571fdda1329618df34247d3cd
Michael Kapelko <kornerr@gmail.com>
2015-10-24 18:06:13 +0700

    #2302 Make whac-a-mole like game and describe how to make it from scratch - Provider

--- scripts/Button.py
+++ scripts/Button.py
@@ -33,11 +33,13 @@ class ButtonImpl(object):
         self.scene  = None
         self.action = None
         self.senv   = None
-    def onActionState(self, actionName, state):
+    def onActionState(self, key, values):
+        state = (values[0] == "1")
         # Ignore activation.
         if (state):
             return
         # Ignore other actions.
+        actionName = key.replace(".active", "")
         if (actionName not in self.actions):
             return
         # Report.
@@ -47,7 +49,13 @@ class ButtonImpl(object):
         self.senv.reportStateChange(st)
         st.set(key, "0")
         self.senv.reportStateChange(st)
-    def onSelection(self, sceneName, nodeName):
+    def onSelection(self, key, values):
+        nodeName = values[0]
+        # Ignore deselection.
+        if (not len(nodeName)):
+            return
+        v = key.split(".")
+        sceneName = v[1]
         node = sceneName + "." + nodeName
         if (node not in self.selectable):
             return
@@ -56,7 +64,11 @@ class ButtonImpl(object):
         st.set("{0}.node".format(bs.press), node)
         st.set("{0}.active".format(bs.press), "1")
         self.action.setState(st)
-    def setSelectable(self, sceneName, nodeName, state):
+    def setSelectable(self, key, values):
+        v = key.split(".")
+        sceneName = v[1]
+        nodeName  = v[2]
+        state     = (values[0] == "1")
         node = sceneName + "." + nodeName
         if (state):
             key = "{0}.{1}.{2}.clone".format(BUTTON_ACTION_PRESS_TYPE,
@@ -73,94 +85,33 @@ class ButtonImpl(object):
             bs = self.selectable[node]
             del self.actions[bs.down]
             del self.selectable[node]
-
-class ButtonListenerAction(pymjin2.ComponentListener):
-    def __init__(self, impl):
-        pymjin2.ComponentListener.__init__(self)
-        # Refer.
-        self.impl = impl
-    def __del__(self):
-        # Derefer.
-        self.impl = None
-    def onComponentStateChange(self, st):
-        for k in st.keys:
-            actionName = k.replace(".active", "")
-            state = (st.value(k)[0] == "1")
-            self.impl.onActionState(actionName, state)
-
-class ButtonListenerSelection(pymjin2.ComponentListener):
-    def __init__(self, impl):
-        pymjin2.ComponentListener.__init__(self)
-        # Refer.
-        self.impl = impl
-    def __del__(self):
-        # Derefer.
-        self.impl = None
-    def onComponentStateChange(self, st):
-        for k in st.keys:
-            v = k.split(".")
-            sceneName = v[1]
-            nodeName = st.value(k)[0]
-            # Ignore deselection.
-            if (not len(nodeName)):
-                continue
-            self.impl.onSelection(sceneName, nodeName)
-
-class ButtonExtensionScriptEnvironment(pymjin2.Extension):
-    def __init__(self, impl):
-        pymjin2.Extension.__init__(self)
-        # Refer.
-        self.impl = impl
-    def deinit(self):
-        # Derefer.
-        self.impl = None
-        print "ButtonExt.deinit"
-    def description(self):
-        return "Turn any node into a simple button"
-    def keys(self):
-        return ["button...selectable",
-                "button...selected"]
-    def name(self):
-        return "ButtonExtensionScriptEnvironment"
-    def set(self, key, value):
-        v = key.split(".")
-        sceneName = v[1]
-        nodeName  = v[2]
-        self.impl.setSelectable(sceneName, nodeName, value == "1")
+    def setSelected(self, key, values):
+        print "setSelected", key, values
 
 class Button:
     def __init__(self, scene, action, scriptEnvironment):
-        # Refer.
-        self.scene  = scene
-        self.action = action
-        self.senv   = scriptEnvironment
         # Create.
-        self.impl              = ButtonImpl(scene, action, scriptEnvironment)
-        self.listenerAction    = ButtonListenerAction(self.impl)
-        self.listenerSelection = ButtonListenerSelection(self.impl)
-        self.extension         = ButtonExtensionScriptEnvironment(self.impl)
+        self.impl = ButtonImpl(scene, action, scriptEnvironment)
+        self.subs = pymjin2.Subscriber()
+        self.prov = pymjin2.Provider(scriptEnvironment,
+                                     "Button",
+                                     "Turn any node into button")
         # Prepare.
+        # Listen to node selection.
         key = "selector..selectedNode"
-        self.scene.addListener([key], self.listenerSelection)
+        self.subs.subscribe(scene, key, self.impl, "onSelection")
         # Listen to button down state.
         key = "{0}..{1}.active".format(BUTTON_ACTION_DOWN_TYPE,
                                        BUTTON_ACTION_DOWN_NAME)
-        self.action.addListener([key], self.listenerAction)
-        self.senv.addExtension(self.extension)
+        self.subs.subscribe(action, key, self.impl, "onActionState")
+        # Provide "button" API.
+        self.prov.provide("button...selectable", self.impl, "setSelectable")
+        self.prov.provide("button...selected",   self.impl, "setSelected")
         print "{0} Button.__init__".format(id(self))
     def __del__(self):
-        # Tear down.
-        self.action.removeListener(self.listenerAction)
-        self.scene.removeListener(self.listenerSelection)
-        self.senv.removeExtension(self.extension)
         # Destroy.
-        del self.listenerAction
-        del self.listenerSelection
-        del self.extension
+        del self.subs
+        del self.prov
         del self.impl
-        # Derefer.
-        self.scene  = None
-        self.action = None
-        self.senv   = None
         print "{0} Button.__del__".format(id(self))
 
--- scripts/Main.py
+++ scripts/Main.py
@@ -1,8 +1,7 @@
 
 import pymjin2
 
-#MAIN_DEPENDENCY_BUTTON   = "scripts/Button.py"
-MAIN_DEPENDENCY_BUTTON   = "scripts/Button2.py"
+MAIN_DEPENDENCY_BUTTON   = "scripts/Button.py"
 MAIN_DEPENDENCY_CRANE    = "scripts/Crane.py"
 MAIN_DEPENDENCY_EXCHANGE = "scripts/Exchange.py"
 MAIN_DEPENDENCY_LINE     = "scripts/Line.py"
